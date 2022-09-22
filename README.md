700a24326b07d2b0d60ba3c7a302ecdf68422109
Hans Castro <hanscastroadmn@gmail.com>
2022-09-22 07:26:44 +0200

    [Tests] Added dense nn test

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This project is just to investigate and make some tests.
 # Webography
 
 * https://www.CryptoDataDownload.com - Download data for mdoels
+* https://www.tensorflow.org/api_docs/python/tf/keras/layers/Dense
+* https://machinelearningmastery.com/tutorial-first-neural-network-python-keras/
--- Tests/Dense_NN_Test.py
+++ Tests/Dense_NN_Test.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras import Input
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 from tensorflow.keras.models import Sequential
 
@@ -17,7 +18,7 @@ scaled_close = scaler.fit_transform(raw_xmr_bust['close'].values.reshape(-1, 1))
 
 # Create train data
 
-prediction_registers = 15
+prediction_registers = 30
 
 x_train, y_train = [], []
 
@@ -34,22 +35,15 @@ x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
 model = Sequential()
 
-model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
-model.add(Dropout(0.2))
-model.add(LSTM(units=50, return_sequences=True))
-model.add(Dropout(0.2))
-model.add(LSTM(units=50))
-model.add(Dropout(0.2))
-model.add(Dense(units=1))
+model.add(Input(shape=(30,)))
+model.add(Dense(60, activation='relu'))
+model.add(Dense(60, activation='relu'))
+model.add(Dense(60, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
 
-model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(x_train, y_train, epochs=25, batch_size=32)
-
-
-# Save model
-
-model.save_weights('weights')
-
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, epochs=30, batch_size=10)
 
 # Testing the model
 
@@ -73,17 +67,23 @@ x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 prediction_prices_min = model.predict(x_test)
 prediction_prices = scaler.inverse_transform(prediction_prices_min)
 
+# calculate error metrics
+
 error = []
 for i in range(len(prediction_prices_min)):
-    error.append(abs(scaled_close[i] - prediction_prices_min[i]))
+    error.append(abs((scaled_close[i] - prediction_prices_min[i])/scaled_close[i]) * 100)
 
 error_average = sum(error)/len(error)
 
 print("Error average: " + str(error_average))
+print("Max error: " + str(max(error)))
+print("Min error: " + str(min(error)))
+
+# Print plot
 
 plt.plot(actual_data, color='black', label='Actual Prices')
 plt.plot(prediction_prices, color='green', label='Predicted Prices')
-plt.plot(error, color='red', label='Error')
+# plt.plot(error, color='red', label='Error')
 plt.title('Price prediction')
 plt.xlabel('Time')
 plt.ylabel('Price')
